7dec486d19f286a42d0c8996d424ef07a8b62953
João Paulo Bassinello <joao.bassinello@tecsinapse.com.br>
2016-03-02 09:02:01 -0300

    Removing mandrill dependency

--- src/main/java/br/com/jpb/model/entity/ContactUs.java
+++ src/main/java/br/com/jpb/model/entity/ContactUs.java
@@ -59,8 +59,10 @@ public class ContactUs implements Serializable {
 
 	@Column(name = "EMAIL")
 	@NotEmpty(message = "Forneça um endereço de e-mail válido")
-	@Size(max = 255, message = "Forneça um endereço de e-mail com no máximo {max} caracteres")
-	@Pattern(regexp = ValidationUtil.REGEXP_EMAIL, message = "Forneça um endereço de e-mail válido")
+	@Size(max = 255, message = "Forneça um endereço de e-mail com no máximo "
+			+ "{max} caracteres")
+	@Pattern(regexp = ValidationUtil.REGEXP_EMAIL, message = "Forneça um " +
+			"endereço de e-mail válido")
 	public String getEmail() {
 		return email;
 	}
@@ -71,7 +73,8 @@ public class ContactUs implements Serializable {
 
 	@Column(name = "NAME")
 	@NotEmpty(message = "Forneça um nome válido")
-	@Size(max = 255, message = "Forneça um nome com no máximo {max} caracteres")
+	@Size(max = 255, message = "Forneça um nome com no máximo {max} " +
+			"caracteres")
 	public String getName() {
 		return name;
 	}
@@ -82,7 +85,8 @@ public class ContactUs implements Serializable {
 
 	@Column(name = "SUBJECT")
 	@NotEmpty(message = "Forneça um assunto válido")
-	@Size(max = 255, message = "Forneça um nome com no máximo {max} caracteres")
+	@Size(max = 255, message = "Forneça um assunto com no máximo {max} " +
+			"caracteres")
 	public String getSubject() {
 		return subject;
 	}
@@ -93,7 +97,8 @@ public class ContactUs implements Serializable {
 
 	@Column(name = "TEXT")
 	@NotEmpty(message = "Forneça uma mensagem válida")
-	@Size(max = 10000, message = "Forneça uma mensagem com no máximo {max} caracteres")
+	@Size(max = 10000, message = "Forneça uma mensagem com no máximo {max} " +
+			"caracteres")
 	public String getText() {
 		return text;
 	}
@@ -135,7 +140,7 @@ public class ContactUs implements Serializable {
 
 	@Override
 	public String toString() {
-		return ToStringBuilder.reflectionToString(this,
-				ToStringStyle.MULTI_LINE_STYLE);
+		return ToStringBuilder.reflectionToString(this, ToStringStyle
+				.MULTI_LINE_STYLE);
 	}
 }
